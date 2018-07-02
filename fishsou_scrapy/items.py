69901aee1683fbc17a4caefbeb5bb32d64aab730
lihang <1943759704@qq.com>
2018-07-02 22:48:10 +0800

    shuichan demo

--- fishsou_scrapy/items.py
+++ fishsou_scrapy/items.py
@@ -10,5 +10,4 @@ import scrapy
 
 class FishsouScrapyItem(scrapy.Item):
     # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+    like = scrapy.Field()
--- fishsou_scrapy/spiders/shuichan.py
+++ fishsou_scrapy/spiders/shuichan.py
@@ -1,11 +1,31 @@
 # -*- coding: utf-8 -*-
 import scrapy
-
+from fishsou_scrapy.items import FishsouScrapyItem
 
 class ShuichanSpider(scrapy.Spider):
     name = 'shuichan'
     allowed_domains = ['shuichan.cc']
-    start_urls = ['http://shuichan.cc/']
+
+    url = "http://www.shuichan.cc/news_list.asp?action=&c_id=91&s_id=101&page="
+    offset = 0
+
+    start_urls = [url + str(offset)]
 
     def parse(self, response):
-        pass
+        #for each in response.css("a[href*=news_view]::attr(href)").extract():
+
+        html = response.xpath('//td[contains(@width, "672")]')
+
+        for each in response.xpath('//td[contains(@width, "672")]'):
+
+            item = FishsouScrapyItem()
+            item['like'] = str(each.css("a[href*=news_view]::attr(href)"))
+
+            yield item
+
+        if self.offset < 10:
+            self.offset += 1
+
+        # 每次处理完一页的数据之后，重新发送下一页页面请求
+        # self.offset自增10，同时拼接为新的url，并调用回调函数self.parse处理Response
+        yield scrapy.Request(self.url + str(self.offset), callback=self.parse)
